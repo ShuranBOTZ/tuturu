99c1a18a913a04375d9b860df4548daba7978974
DaniXd <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-11-02 17:50:37 +0800

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -2,8 +2,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
 let text = `
 「 Suport Bot Dengan Cara donasi 」
 
-⫹⫺ Via Pulsa Indosat : 6285850539404
-⫹⫺ Via Dana : 6285850539404
+⫹⫺ Via Pulsa Telkomsel : 6281251549899
+⫹⫺ Via Dana : 6281251549899
 ⫹⫺ Via Saweria :
 ⫹⫺ https://saweria.co/jarotoffc
 
